f5b457a6116e05a6fde39665eba0e4916e287f0d
cbwolfe94 <cbwolfe94@hotmail.com>
2025-05-09 12:23:51 -0500

    Add skeleton code

---
+++ src/app/application/application.c
@@ -0,0 +1,14 @@
+#include "application.h"
+
+void applicationInit(void)
+{
+    // Things like HAL init and task init
+}
+
+void applicationRun(void)
+{
+    // Start the scheduler for FreeRTOS
+
+    // Should never get here
+    while (1) {}
+}
---
+++ src/app/application/application.h
@@ -0,0 +1,7 @@
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+void applicationInit(void);
+void applicationRun(void);
+
+#endif // APPLICATION_H
